bcb03dd04082a58ef2d280c962c93202e7cb4234
Rajasekhar Madabattula <rajamadabattula@gmail.com>
2021-07-01 09:35:07 +0530

    Checking the dynamic SQL Query

---
+++ dags/udac_example_dag.py
@@ -0,0 +1,163 @@
+
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                               LoadDimensionOperator, DataQualityOperator)
+from airflow.operators.postgres_operator import PostgresOperator
+from helpers import SqlQueries
+from sparkify_dimension_subdag import load_dimension_subdag
+from airflow.operators.subdag_operator import SubDagOperator
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+s3_bucket = 'udacity-dend'
+song_s3_key = "song_data"
+log_s3_key = "log_data"
+log_json_file = "log_json_path.json"
+
+default_args = {
+    'owner': 'udacity',
+    'depends_on_past': True,
+    'start_date': datetime(2019, 1, 12),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': True
+}
+
+dag_name = 'udac_example_dag'
+dag = DAG(dag_name,
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *',
+          max_active_runs=1
+          )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+create_tables = PostgresOperator(
+    task_id="create_tables",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql="create_tables.sql"
+)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    table='staging_events',
+    s3_bucket='udacity-dend',
+    s3_key='log_data',
+    copy_json_option='s3://udacity-dend/log_json_path.json',
+    region='us-west-2',
+    dag=dag
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    table='staging_songs',
+    s3_bucket='udacity-dend',
+    s3_key='song_data',
+    copy_json_option='auto',
+    region='us-west-2',
+    dag=dag
+)
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    redshift_conn_id='redshift',
+    sql_statement=f""" 
+        INSERT INTO public.songplays(playid,start_time,userid, level, songid, artistid, sessionid,location,useragent ) 
+        {SqlQueries.songplay_table_insert}""",
+    dag=dag
+)
+
+
+load_user_dimension_table = SubDagOperator(
+    subdag=load_dimension_subdag(
+        parent_dag_name=dag_name,
+        task_id="Load_user_dim_table",
+        redshift_conn_id="redshift",
+        start_date=default_args['start_date'],
+        sql_statement=f""" 
+            INSERT INTO public.user (userid,first_name,last_name, gender, level) 
+            {SqlQueries.user_table_insert}""",
+        delete_load=True,
+        table_name="users",
+    ),
+    task_id="Load_user_dim_table",
+    dag=dag,
+)
+
+
+load_song_dimension_table = SubDagOperator(
+    subdag=load_dimension_subdag(
+        parent_dag_name=dag_name,
+        task_id="Load_song_dim_table",
+        redshift_conn_id="redshift",
+        start_date=default_args['start_date'],
+        sql_statement=f""" 
+            INSERT INTO public.user (song_id, title, artist_id, year, duration) 
+            {SqlQueries.song_table_insert}""",
+        delete_load=True,
+        table_name="songs",
+    ),
+    task_id="Load_song_dim_table",
+    dag=dag,
+)
+
+
+load_artist_dimension_table = SubDagOperator(
+    subdag=load_dimension_subdag(
+        parent_dag_name=dag_name,
+        task_id="Load_artist_dim_table",
+        redshift_conn_id="redshift",
+        start_date=default_args['start_date'],
+        sql_statement=f""" 
+            INSERT INTO public.user (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) 
+            {SqlQueries.artist_table_insert}""",
+        delete_load=True,
+        table_name="artists",
+    ),
+    task_id="Load_artist_dim_table",
+    dag=dag,
+)
+
+
+load_time_dimension_table = SubDagOperator(
+    subdag=load_dimension_subdag(
+        parent_dag_name=dag_name,
+        task_id="Load_time_dim_table",
+        redshift_conn_id="redshift",
+        start_date=default_args['start_date'],
+        sql_statement=f""" 
+            INSERT INTO public.user (start_time,hour,day, week, month,year, weekday) 
+            {SqlQueries.time_table_insert}""",
+        delete_load=True,
+        table_name="time",
+    ),
+    task_id="Load_time_dim_table",
+    dag=dag,
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    redshift_conn_id='redshift',
+    test_query='select count(*) from songs where songid is null;',
+    expected_result=0,
+    dag=dag
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+create_tables >> start_operator
+start_operator >> stage_events_to_redshift >> load_songplays_table
+start_operator >> stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_user_dimension_table >> run_quality_checks
+load_songplays_table >> load_song_dimension_table >> run_quality_checks
+load_songplays_table >> load_artist_dimension_table >> run_quality_checks
+load_songplays_table >> load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
